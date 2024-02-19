938183b5f54ae983d6e7dc6523d8283641749a67
aminoxlhd <aminoxlhd@gmail.com>
2024-02-19 15:49:34 -0800

    add 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user_url = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todo = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
     comp = [t.get("title") for t in todo if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user_url.get("name"), len(comp), len(todo)))
