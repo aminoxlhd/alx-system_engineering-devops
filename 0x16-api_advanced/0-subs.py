b994871b6da9807676d3ce2523025fca0af4c8f0
aminoxlhd <aminoxlhd@gmail.com>
2024-03-05 23:57:19 +0000

    add 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    url = 'https://www.reddit.com/r/{subreddit}/about.json'
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'My_User_Agent'}
     r = requests.get(url, headers=headers)
     if r.status_code == 200:
