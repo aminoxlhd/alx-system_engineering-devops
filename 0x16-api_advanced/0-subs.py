89bfa019278f9fe1b752921f76d1aa5f3c99553b
aminoxlhd <aminoxlhd@gmail.com>
2024-03-06 01:11:05 +0000

    edit 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""queries the Reddit API and returns the number of subscribers"""
+""" queries  the Reddit API and returns the number of subscribers """
 import requests
 
 
 def number_of_subscribers(subreddit):
-	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-	headers = {"User-Agent": "Mozilla/5.0"}
-	r = requests.get(url, headers=headers)
-	if r.status_code == 404:
-		return 0
-	else:
-                data = r.json()
-                sub = data['data']['subscribers']
-                return sub
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code == 200:
+        data = r.json()
+        sub = data['data']['subscribers']
+        return sub
+    else:
+        return 0
